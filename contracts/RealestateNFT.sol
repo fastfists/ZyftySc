1a6b0bfa95e441ea3ae73d3f804970976102de54
Denzell Ford <fdenzell@gmail.com>
2022-03-17 11:42:07 -0500

    initial commit

---
+++ contracts/RealestateNFT.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.1;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract RealestateNFT is ERC721 {
+    constructor(string memory name, string memory symbol)
+        ERC721(name, symbol)
+    {}
+}
