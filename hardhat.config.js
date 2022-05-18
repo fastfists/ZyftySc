c5bfa40de0caeac40aad56db05a9c27c8fc2955d
Denzell Ford <fdenzell@gmail.com>
2022-05-17 23:40:52 -0500

    updated hardhat config and package.json

--- hardhat.config.js
+++ hardhat.config.js
@@ -19,4 +19,16 @@ module.exports = {
         cache: "./cache",
         artifacts: "./artifacts",
     },
+  networks: {
+        acalaTest: {
+            url: "https://tc7-eth.aca-dev.network",
+            chainId: 595,
+            // Development built-in default deployment account
+            accounts: ["0xa872f6cbd25a0e04a08b1e21098017a9e6194d101d75e13111f71410c59cd57f"]
+      },
+      matic: {
+          url: "https://rpc-mumbai.maticvigil.com",
+          accounts: ["46e0483c6cb7a3b10d643a4ecc643633b5c89f73756d7921511fa2ae3bb8040e", "29784bc06418dcf832968aba1f4580b5c96b32789098e60ac1f41a698c9c3086", "4a9241c5e34cbb3605e69fb781b7391d799de131cdc8ba0c133076789f4f8933"]
+      }
+  }
 };
